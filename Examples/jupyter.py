226374aa99715bef826f92b8476535608fe92def
Connor Ferster <connorferster@gmail.com>
2022-02-08 22:11:38 -0800

    Progress on 2022 Engineering edition

---
+++ Examples/jupyter.py
@@ -0,0 +1,33 @@
+"""
+Contains helper functions for performing calculations in Jupyter Notebook.
+Use dir(rjc.jupyter) for a directory of the functions.
+
+Connor Ferster, cferster@rjc.ca 2021
+"""
+from IPython.display import display
+
+def title_block(proj_name = "", proj_id = "", designer = "", _test=False, **kwargs) -> None:
+    """
+    Returns None. Displays a simple title block for engineering notes created in Jupyter.
+    
+    '_test' argument is used for internal testing: returns the markdown as a string instead
+    of displaying in Jupyter.
+    """
+    import datetime
+    try:
+        from IPython.display import Markdown
+    except ImportError:
+        raise ImportError("title_block() function can only be called from within Jupyter.")
+    todays_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    md = (
+        f"**Project:** {proj_name}<br>\n"
+        f"**Project ID:** {proj_id}<br>\n"
+        f"**Designer:** {designer}<br>\n"
+        f"**Date:** {todays_date}<br>\n"
+    )
+    extra_md = ""
+    for kw, arg in kwargs.items():
+        extra_md += f"**{kw}:** {arg}<br>\n"
+    display(Markdown(md + extra_md))
+    if _test:
+        return md
